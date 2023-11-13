18e587eed6e58989aec0aff813f604d0d6770e33
Alberto Bollo <albertobollo7@protonmail.com>
2023-11-14 00:26:56 +0100

    Actualizacion de ejercicios

---
+++ Ejercicios/Ejercicio_empresa.sql
@@ -0,0 +1,13 @@
+
+create database empresa;
+
+use empresa;
+
+
+create table empleado(
+    cod_e varchar(10),
+    nombre varchar(30),
+    fecha_incorporacion varchar(30),
+    primary key(cod_e)
+
+);
