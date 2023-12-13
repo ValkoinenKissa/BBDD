b3d9ad5372105e7727d419a57868823805940134
Alberto Bollo <albertobollo7@protonmail.com>
2023-12-13 01:35:06 +0100

    Actualizacion nuevos ejercicios

---
+++ Proyecto_BBDD/SQL/trabajo.sql
@@ -0,0 +1,136 @@
+create database tiendaOnline;
+use tiendaOnline;
+
+create table categoria(
+
+    id_categoria numeric(4) primary key,
+
+    nombre_categoria varchar(30) unique,
+
+    descripcion varchar(60)
+
+    );
+
+create table producto(
+
+    id_producto numeric(4) primary key,
+
+    nombre_producto varchar(25) unique,
+
+    precio decimal(7,2),
+
+    fabricante varchar(30),
+
+    stock enum( 'S' , 'N' ),
+
+    descripcion varchar(60),
+
+    id_categoria numeric(4),
+
+    foreign key (id_categoria) references categoria(id_categoria) on delete cascade on update cascade
+
+    );
+
+create table agencia_de_paqueteria(
+
+    CIF varchar(9) primary key,
+
+    nombre varchar(20)
+
+    );
+
+create table pedido(
+
+    CIF varchar(9),
+
+    id_pedido numeric(4),
+
+    metodo_pago varchar(30),
+
+    fecha_pedido date,
+
+    estado enum( 'Preparacion' , 'Enviado' , 'Entregado' ),
+
+    foreign key (CIF) references agencia_de_paqueteria(CIF) on delete cascade on update cascade,
+
+    primary key(CIF,id_pedido)
+
+    );
+
+create table cliente(
+
+    id_cliente numeric(4) primary key,
+
+    usuario varchar(20) unique,
+
+    correo varchar(30),
+
+    telefono numeric(9),
+
+    direccion varchar(30)
+
+    );
+
+create table compra(
+
+    id_producto numeric(4),
+
+    id_cliente numeric(4),
+
+    CIF numeric(7),
+
+    id_pedido numeric(4),
+
+    foreign key (id_producto) references producto(id_producto) on delete cascade on update cascade,
+
+    foreign key (id_cliente) references cliente(id_cliente) on delete cascade on update cascade,
+
+    foreign key (CIF,id_pedido) references pedido(CIF,id_pedido) on delete cascade on update cascade,
+
+    primary key(id_producto,CIF,id_pedido)
+
+    );
+
+create table empleado(
+
+    id_empleado numeric(4) primary key,
+
+    salario decimal(6,2),
+
+    nombre varchar(15),
+    
+    fecha_incorporacion date,
+
+    constraint salariominimo check(salario > 1050)
+
+    );
+
+create table supervisa(
+
+    id_empleado_supervisa numeric(4),
+
+    id_empleado_supervisado numeric(4),
+
+    foreign key (id_empleado_supervisa) references empleado(id_empleado) on delete cascade on update cascade,
+
+    foreign key (id_empleado_supervisado) references empleado(id_empleado) on delete cascade on update cascade,
+
+    primary key(id_empleado_supervisa,id_empleado_supervisado)
+
+    );
+
+create table gestion(
+
+    id_empleado numeric(4),
+
+    CIF numeric(7),
+
+    id_pedido numeric(4),
+
+    foreign key (id_empleado) references empleado(id_empleado) on delete cascade on update cascade,
+
+    foreign key (CIF,id_pedido) references pedido(CIF,id_pedido) on delete cascade on update cascade,
+
+    primary key(id_empleado,CIF,id_pedido)
+
+    );
--- Proyecto_BBDD/automatizacion_inserts/v.2.0/scriptInsertsV2.py
+++ Proyecto_BBDD/automatizacion_inserts/v.2.0/scriptInsertsV2.py
@@ -1,89 +1,234 @@
 import random
 
-abecedariomax = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-abecedariomin = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-abecedarionumerico = ["1","2","3","4","5","6","7","8","9"]
+abecedario_max = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+abecedario_min = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+abecedario_numerico = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-print("************************************Categoria********************************************")
+print(
+    "************************************Categoria********************************************"
+)
 
-#Insert categoria
-categoria = ["Smartphones","Portatiles","Ordenadores","Perifericos","Monitores","Componentes","Consolas","Videojuegos","Patinetes","Televisiones"]
-descripciones = ["Telefonos inteligentes de ultima generacion","Portatiles potentes con el mejor precio del mercado","Los ordenadores mas potentes del mercado","Los ultimos perifericos del mercado","Los monitores con mejor resolucion","Los componentes mas potentes","Ultimas novedades en consolas","Los videojuegos mas divertidos","Los patinetes mas rapidos","Las televisiones mas grandes"]
+# Insert categoria
+categoria = [
+    "Smartphones",
+    "Portatiles",
+    "Ordenadores",
+    "Perifericos",
+    "Monitores",
+    "Componentes",
+    "Consolas",
+    "Videojuegos",
+    "Patinetes",
+    "Televisiones",
+]
+descripciones = [
+    "Telefonos inteligentes de ultima generacion",
+    "Portatiles potentes con el mejor precio del mercado",
+    "Los ordenadores mas potentes del mercado",
+    "Los ultimos perifericos del mercado",
+    "Los monitores con mejor resolucion",
+    "Los componentes mas potentes",
+    "Ultimas novedades en consolas",
+    "Los videojuegos mas divertidos",
+    "Los patinetes mas rapidos",
+    "Las televisiones mas grandes",
+]
 
 fkIdCategoria = []
 
-for i in range (0,10):
+for i in range(0, 10):
     idCategoria = random.randint(1000, 9999)
     if idCategoria == fkIdCategoria:
         idCategoria = random.randint(1000, 9999)
-        print(f"insert into categoria values{idCategoria, categoria[i], descripciones[i]};")
+        print(
+            f"insert into categoria values{idCategoria, categoria[i], descripciones[i]};"
+        )
     else:
-        print(f"insert into categoria values{idCategoria, categoria[i], descripciones[i]};")
+        print(
+            f"insert into categoria values{idCategoria, categoria[i], descripciones[i]};"
+        )
     fkIdCategoria.append(idCategoria)
 
-print("************************************Producto********************************************")
+print(
+    "************************************Producto********************************************"
+)
 
-#Insert producto
+# Insert producto
 nombreprod = "n"
 descripcion = "d"
-fabricantes = ["LG","Samsung","Philips","Appel","MSI","Razer","Xiaomi"]
-stock = ["S","N"]
+fabricantes = ["LG", "Samsung", "Philips", "Appel", "MSI", "Razer", "Xiaomi"]
+stock = ["S", "N"]
 
 arrayIdProducto = []
 
-for i in range(0,30):
+for i in range(0, 30):
     idprodcuto = random.randint(1000, 9999)
     precio = random.uniform(0, 9999)
     redondeado = round(precio, 2)
-    fabricantess = fabricantes[random.randint(0,6)]
-    prodlength = random.randint(0,2)
+    fabricantess = fabricantes[random.randint(0, 6)]
+    prodlength = random.randint(0, 2)
 
     if prodlength == 0:
-        usuariorandom =fabricantess+ "-" +  abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
+        usuariorandom = (
+                fabricantess
+                + "-"
+                + abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
     elif prodlength == 1:
-        usuariorandom = fabricantess+ "-" +  abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
+        usuariorandom = (
+                fabricantess
+                + "-"
+                + abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
     else:
-        usuariorandom = fabricantess+ "-" +  abecedariomax[random.randint(0,25)] + abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
-    
+        usuariorandom = (
+                fabricantess
+                + "-"
+                + abecedario_max[random.randint(0, 25)]
+                + abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
+
     if idprodcuto == arrayIdProducto:
-        print(f"insert into producto values{idprodcuto,usuariorandom,redondeado,fabricantess,stock[random.randint(0,1)],descripcion,fkIdCategoria[random.randint(0,9)]};")
+        print(
+            f"insert into producto values{idprodcuto, usuariorandom, redondeado, fabricantess, stock[random.randint(0, 1)], descripcion, fkIdCategoria[random.randint(0, 9)]};"
+        )
     else:
         idprodcuto = random.randint(1000, 9999)
-        print(f"insert into producto values{idprodcuto,usuariorandom,redondeado,fabricantess,stock[random.randint(0,1)],descripcion,fkIdCategoria[random.randint(0,9)]};")
+        print(
+            f"insert into producto values{idprodcuto, usuariorandom, redondeado, fabricantess, stock[random.randint(0, 1)], descripcion, fkIdCategoria[random.randint(0, 9)]};"
+        )
 
     arrayIdProducto.append(idprodcuto)
 
+print(
+    "************************************Agencia paquete********************************************"
+)
 
-print("************************************Agencia paquete********************************************")
+# Insert Agencia de Paqueteria
 
-#Insert Agencia de Paqueteria
-
-nombreAgencia = ["Fedex","Correos","DHL","Inpost","UPS","T3C","SEUR","MRV","Amazon","Najesa"]
+nombreAgencia = [
+    "Fedex",
+    "Correos",
+    "DHL",
+    "Inpost",
+    "UPS",
+    "T3C",
+    "SEUR",
+    "MRV",
+    "Amazon",
+    "Najesa",
+]
 
 arrayCIF = []
 
-for i in range (0,10):
+for i in range(0, 10):
     cif = str(random.randint(10000000, 99999999))
-    cifrandom = abecedariomax[random.randint(0,25)] + cif
+    cifrandom = abecedario_max[random.randint(0, 25)] + cif
     if cifrandom == arrayCIF:
         cif = str(random.randint(10000000, 99999999))
-        cifrandom = abecedariomax[random.randint(0,25)] + cif
-        print(f"insert into agencia_de_paqueteria values{cifrandom,nombreAgencia[i]};")
+        cifrandom = abecedario_max[random.randint(0, 25)] + cif
+        print(f"insert into agencia_de_paqueteria values{cifrandom, nombreAgencia[i]};")
     else:
-        print(f"insert into agencia_de_paqueteria values{cifrandom,nombreAgencia[i]};")
+        print(f"insert into agencia_de_paqueteria values{cifrandom, nombreAgencia[i]};")
     arrayCIF.append(cifrandom)
-    
 
-print("************************************Pedidos********************************************")
+print(
+    "************************************Pedidos********************************************"
+)
 
-#Insert Pedidos
+# Insert Pedidos
 
-metodoPago = ["En efectivo","Tarjeta credito/debito","Paypal","Bizum","Contra rembolso","Transferencia bancaria"]
-estado = ["Preparacion","Enviado","Entregado"]
+metodoPago = [
+    "En efectivo",
+    "Tarjeta credito/debito",
+    "Paypal",
+    "Bizum",
+    "Contra rembolso",
+    "Transferencia bancaria",
+]
+estado = ["Preparacion", "Enviado", "Entregado"]
 
 arrayPedido = []
 
-for i in range (0,40):
+for i in range(0, 40):
     idPedido = random.randint(1000, 9999)
     anno = str(random.randint(2000, 2023))
     mes = str(random.randint(1, 12))
@@ -91,39 +236,106 @@ for i in range (0,40):
     fecha = anno + "-" + mes + "-" + dia
     if idPedido == arrayPedido:
         idPedido = random.randint(1000, 9999)
-        print(f"insert into pedido values{arrayCIF[random.randint(0,9)],idPedido,metodoPago[random.randint(0,5)],fecha,estado[random.randint(0,2)]};")
+        print(
+            f"insert into pedido values{arrayCIF[random.randint(0, 9)], idPedido, metodoPago[random.randint(0, 5)], fecha, estado[random.randint(0, 2)]};"
+        )
     else:
-        print(f"insert into pedido values{arrayCIF[random.randint(0,9)],idPedido,metodoPago[random.randint(0,5)],fecha,estado[random.randint(0,2)]};")
+        print(
+            f"insert into pedido values{arrayCIF[random.randint(0, 9)], idPedido, metodoPago[random.randint(0, 5)], fecha, estado[random.randint(0, 2)]};"
+        )
     arrayPedido.append(idPedido)
 
-print("************************************Clientes********************************************")
+print(
+    "************************************Cliente********************************************"
+)
 
-#Insert cliente
+# Insert cliente
 
 usuario = "u"
 direccion = "d"
-correo = ["@gmail.com","@hotmail.com","@outlook.com","@icloud.com"]
+correo = ["@gmail.com", "@hotmail.com", "@outlook.com", "@icloud.com"]
 
 arrayCliente = []
 
-for i in range(0,30):
-    idCliente = random.randint(1000,9999)
-    telefono = random.randint(600000000,999999999)
-    usuariolength = random.randint(0,2)
+for i in range(0, 30):
+    idCliente = random.randint(1000, 9999)
+    telefono = random.randint(600000000, 999999999)
+    usuariolength = random.randint(0, 2)
 
     if usuariolength == 0:
-        usuariorandom = abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
+        usuariorandom = (
+                abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
     elif usuariolength == 1:
-        usuariorandom = abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
+        usuariorandom = (
+                abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
     else:
-        usuariorandom = abecedariomax[random.randint(0,25)] + abecedariomax[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)] + abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)] +abecedariomin[random.randint(0,25)]+ abecedariomin[random.randint(0,25)]+ abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)] + abecedarionumerico[random.randint(0,8)]
-    correousuario = usuariorandom + correo[random.randint(0,3)]
+        usuariorandom = (
+                abecedario_max[random.randint(0, 25)]
+                + abecedario_max[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_min[random.randint(0, 25)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+                + abecedario_numerico[random.randint(0, 8)]
+        )
+    correousuario = usuariorandom + correo[random.randint(0, 3)]
     if idCliente == arrayCliente:
-        idCliente = random.randint(1000,9999)
-        print(f"insert into cliente values{idCliente,usuariorandom,correousuario,telefono,direccion};")
+        idCliente = random.randint(1000, 9999)
+        print(
+            f"insert into cliente values{idCliente, usuariorandom, correousuario, telefono, direccion};"
+        )
     else:
-        print(f"insert into cliente values{idCliente,usuariorandom,correousuario,telefono,direccion};")
+        print(
+            f"insert into cliente values{idCliente, usuariorandom, correousuario, telefono, direccion};"
+        )
     arrayCliente.append(idCliente)
 
+print(
+    "*****************************************Compra**********************************"
+)
 
-print("********************************************************************************")
+# Insert compra
+
+"""
+Los inserts de a tabla compra se componen unicamente de las foreing keys de las tablas, producto, cliente
+y pedido
+"""
+
+# Tabla empleado
+
+nombre_emp = "nom"
+
+for i in range(0, 20):
+    anno = str(random.randint(2000, 2023))
+    mes = str(random.randint(1, 12))
+    dia = str(random.randint(1, 31))
+    fecha_incorporacion = anno + "-" + mes + "-" + dia
+    salario = random.randint(1050, 4500)
+    id_empleado = random.randint(1000, 9999)
+
+    print(f"insert into empleado values {id_empleado, salario, nombre_emp, fecha_incorporacion}")
+
+
+
+#Tabla supervisa
