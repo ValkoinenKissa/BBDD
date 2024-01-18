d6cc1f1dea94965ac06245d4a20de1171f53ac8d
Alberto Bollo <albertobollo7@protonmail.com>
2024-01-18 18:55:03 +0100

    Actualizacion nuevos ejercicios

--- Ejercicios/Selects unitabla/selects.sql
+++ Ejercicios/Selects unitabla/selects.sql
@@ -108,3 +108,137 @@ SELECT nombre_empleado, comision_empleado, numero_hijos_empleado FROM t_empleado
 
 SELECT nombre_empleado, comision_empleado, numero_hijos_empleado 
 FROM t_empleados WHERE LEFT(nombre_empleado, LOCATE(',', nombre_empleado) - 1) LIKE '%a%a%'AND (numero_hijos_empleado = 0 OR comision_empleado IS NULL);
+
+
+
+SELECT nombre_empleado, comision_empleado, numero_hijos_empleado FROM t_empleados WHERE substring_index(nombre_empleado, ',', -1) like "%a%%a%" and (numero_hijos_empleado = 0 or comision_empleado IS NULL);
+
+SELECT nombre_empleado, salario_base_empleado AS salario FROM t_empleados WHERE salario_base_empleado < 2200 AND (substring_index(nombre_empleado, ',', 1) LIKE 'M%' OR substring_index(nombre_empleado, ',', -1) LIKE '%Z') ORDER BY nombre_empleado;
+
+SELECT codigo_empleado, extension_telefonica_empleado, nombre_empleado FROM t_empleados where abs(codigo_empleado - extension_telefonica_empleado) < 100 AND substring_index(nombre_empleado, ',', 1) NOT LIKE 'L%';
+
+SELECT comision_empleado, nombre_empleado, salario_base_empleado FROM t_empleados WHERE numero_hijos_empleado > 3 ORDER BY comision_empleado, nombre_empleado;
+
+
+SELECT nombre_empleado, fecha_nacimiento_empleado, fecha_ingreso_empleado FROM t_empleados WHERE SUBSTRING_INDEX(nombre_empleado, ',', 1) LIKE "%a%%r%" AND (YEAR(fecha_nacimiento_empleado) > 1960 OR YEAR(fecha_ingreso_empleado) > 1987);
+
+SELECT nombre_empleado, comision_empleado, numero_hijos_empleado FROM t_empleados WHERE nombre_empleado LIKE "%a% %a%" AND numero_hijos_empleado = 0 OR comision_empleado IS NULL;
+
+SELECT nombre_empleado, comision_empleado, numero_hijos_empleado 
+
+FROM t_empleados WHERE LEFT(nombre_empleado, LOCATE(',', nombre_empleado) - 1) LIKE '%a%a%'AND (numero_hijos_empleado = 0 OR comision_empleado IS NULL);
+
+
+SELECT distinct producto AS prodlen, precio_unidad  AS coste_total FROM facturas WHERE length(producto) = length(cliente) ORDER BY precio_unidad LIMIT 10;
+
+
+SELECT terminal, cargo, date_add(date_add(finicio, interval 1 month), interval 5 day) AS finicio FROM usa WHERE timestampdiff(DAY, finicio, ffin) < 400;
+
+SELECT substring(ISBN_C, 5) AS tercio, ISBN_C FROM Comic WHERE substring_index(Titulo, " ", -1) like "%1%";
+
+
+SELECT concat(Nombre, " ", "(",especie,")" , " ", Nombre) AS personaje, Nacimiento from Personajes where substring(Nacimiento, 1, 2) = 0 or 5;
+
+
+
+select count(*) from mascotas;
+
+select count(*) from mascotas where fallecimiento is not null;
+
+select count(*) from mascotas where fallecimiento is null;
+
+select count(*) -count(fallecimiento) from mascotas;
+
+select sum(veces_vacunacion) from mascotas;
+
+
+select sum(timestampdiff(year,Nacimiento, curdate())) as años from mascotas where fallecimiento is not null;
+
+
+select concat(Nombre), count(*) from mascotas;
+
+
+select substring(cliente, 1, 4) as cli4, substring(producto, -4) as pro4 from facturas where unidades < 6;
+
+select lower(concat(NOM_AUTOR, " ", APE1_AUTOR)) as Autor, concat(substring(Titulo, 1, 1), lower(substring(Titulo, 2))) as Titulo from libros;
+
+select numerorem, numerodes, date_format(finicio, "%d de %m de %y") as finicio, date_format(ffin, "%d de %m de %y") as fin from llamada where timestampdiff(minute, finicio, ffin) > 10;
+
+select numerorem, numerodes, date_format(finicio, "%H h %i m %s s") as finicio, date_format(ffin, "%H h %i m %s s") as fin from llamada where hour(finicio) between 9 and 11 and hour(ffin) not between 9 and 11;
+
+select terminal, cargo,  date_format(finicio, "%d/%m/%Y") as finicio, date_format(ffin, "%d/%m/%Y") as ffin from usa where timestampdiff(month, finicio, ffin) > 6 and abs(day(finicio) - day(ffin)) < 5;
+
+select terminal, cargo, date_format(date_add(date_add(finicio, interval 1 month), interval 5 day), "%m-%d (%Y)") as finicio from usa where timestampdiff (day, finicio, ffin) < 400;
+
+
+select numerorem, numerodes,  date_format(finicio, "%d de %m de %Y") as finicio,  date_format(ffin, "%d de %m de %Y") as fin from llamada where timestampdiff(minute, finicio, ffin) > 10 and abs(substring(numerorem, -3) -substring(numerodes, -3)) > 3;
+
+SELECT concat(Nombre, " ", "(",especie,")" , " ", Nombre) AS personaje, Nacimiento from Personajes where substring(Nacimiento, 1, 2) = 0 or 5;
+
+select Nombre, Nacimiento from Personajes WHERE right(substring_index(Nacimiento, " ", 1), 1)  LIKE '%0' or (right(substring_index(Nacimiento, " ", 1), 1) like '%5');
+
+select Nombre, Nacimiento, especie from Personajes WHERE right(substring_index(Nacimiento, " ", 1), 1)  LIKE '%0' or (right(substring_index(Nacimiento, " ", 1), 1) like '%5');
+
+select concat(substring_index(Nombre, " ", 1), " ", "(",especie,")", " ", substring_index(Nombre, " ", 1)) as personaje, Nacimiento from Personajes where right(substring_index(Nacimiento, " ", 1), 1)  LIKE '%0' or (right(substring_index(Nacimiento, " ", 1), 1) like '%5');
+
+select ISBN_N, Titulo from Novela where abs(timestampdiff(month, Ultima_edicion, Lanzamiento)) > 6 and locate(substring(ISBN_N, -1), ISBN_N) = 1;
+
+select concat(substring_index(ISBN_C, "-", -4), substring_index(ISBN_C, "-", 4)) from Comic;
+
+select substring(ISBN_C,TRUNCATE(length(ISBN_C) / 3, 0), length(substring(ISBN_C,TRUNCATE(length(ISBN_C) / 3, 0))) -2) as tercio, ISBN_C from Comic where substring(Titulo, -1) = 1 ;
+
+select substring(Titulo, -1) from Comic;
+
+4. Agregación múltiple
+
+select max(nacimiento) as masJoven, min(nacimiento) as masMayor from mascotas where fallecimiento is null;
+
+
+select nombre, nacimiento from mascotas where fallecimiento is null order by nacimiento asc limit 1;
+
+
+select count(exito) as exitos from peticiones where exito = 0;
+
+select date_format(max(tiempo), "%H:%i") as Hora from peticiones where tiempo_procesado > 3 and tipo_peticion like "%consulta%";
+
+select lower(concat(NOM_AUTOR, " ", APE1_AUTOR)) as Autor, concat(upper(substring(Titulo, 1, 1)), lower(substring(Titulo, 2))) as Titulo from libros;
+
+
+select concat(substring(cliente, 1, 4), "(", substring(producto, 1, 5), ")") as "cli(prod)", precio_unidad from facturas where precio_unidad between 0.25 and 1;
+
+select lower(TITULO) as Titulo, concat(upper(substring(NOM_AUTOR, 1, 1)), lower(substring(NOM_AUTOR, 2))) as Autor from libros where NUM_PAGINAS > 200 order by TITULO desc;
+
+select nombre_empleado from t_empleados order by substring(nombre_empleado,locate(",", nombre_empleado)) ;
+
+
+select terminal, cargo, date_format(finicio, "%d/%m/%Y") as finicio, date_format(ffin, "%d/%m/%Y") as ffin from usa where  timestampdiff(day, finicio, ffin)  > 5 and timestampdiff(month, finicio, ffin) > 6;
+
+select Nombre, substring_index(Nacimiento, " ", 1) as "edad en Batalla de Yavin" from Personajes where substring(Nacimiento, -3) like "%ABY%" ;
+
+select Titulo, concat(Titulo,",", date_format(Lanzamiento, "%Y-%m"),":", Id_dir) as identif from Pelicula where timestampdiff(year, "2000-1-1", Lanzamiento) > 13 and substring_index(Titulo, ":", 1) like "%VI%" or substring_index(Titulo, ":", 1) like "%VII%" or substring_index(Titulo, ":", 1) like "%VIII%";
+
+select count(*) especie, especie from  mascotas group by especie;
+
+select count(*) especie, especie, count(*) sexo, sexo from mascotas group by especie, sexo;
+
+select avg(timestampdiff(year, nacimiento, curdate())) as edadMedia, especie from mascotas where nacimiento is null;
+
+select especie, group_concat(nombre) as nombres_mascotas from mascotas group by especie;
+
+select propiertario, count(*) as num_mascotas, group_concat(nombre) as nombres from mascotas where nacimiento is null group by
+
+select especie, sexo, group_concat(distinct propietario) as propietario from mascotas group by especie, sexo;
+
+select group_concat(nombre) as nombre_mascotas, month(nacimiento) as mes_nac from mascotas group by month(nacimiento) order by month(nacimiento);
+
+select group_concat(nombre) as nombre, dayname(nacimiento) as diaNac from mascotas group by diaNac, nombre order by dayname(nacimiento) asc;
+
+
+select substring(Nombre, length(Profesion), length(Planeta)) as codigo from Personajes where length(Nombre) >= length(concat(Planeta, Profesion));
+
+select ISBN_N, Titulo from Novela where timestampdiff(month, Lanzamiento, Ultima_edicion) > 6 and locate(substring(ISBN_N, -1), ISBN_N) = 1;
+
+select substring(ISBN_N, -1) from Novela;
+
+
+select nombre_departamento, presupuesto_departamento,(presupuesto_departamento / 12 * 7.32 /100) + presupuesto_departamento / 10 as incremento  from t_departamentos;
