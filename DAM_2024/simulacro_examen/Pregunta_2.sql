5fa140042fb6fe8723f35a2aba010d991a9ae446
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-25 22:48:22 +0100

    Nuevos .sql simulacro examen

--- DAM_2024/simulacro_examen/Pregunta_2.sql
+++ DAM_2024/simulacro_examen/Pregunta_2.sql
@@ -1,30 +1,9 @@
 /*
- Se pide una tabla resumen que muestre el precio medio real* por el que
- se ha vendido cada producto (* el precio medio
- se tiene que calcular teniendo en cuenta la cantidad de producto vendido)
- */
-select productos.nombre_producto, precio_unitario / detalles_pedido.cantidad as
-precioMedio from productos left join detalles_pedido on productos.id_producto = detalles_pedido.id_producto
-group by productos.nombre_producto;
-
-
-/*
- Se pide una tabla resumen que muestre
- el número de pedidos en los que aparece cada producto
+ 1.	Se pide una tabla resumen que muestre el precio medio real* por el que se ha vendido cada producto
+ (* el precio medio se tiene que calcular teniendo en cuenta la cantidad de producto vendido)
  */
 
-select count(pedidos.id_pedido) as numeroPedidos, productos.nombre_producto from pedidos
-right join detalles_pedido on pedidos.id_pedido = detalles_pedido.id_pedido
-right join productos on detalles_pedido.id_producto = productos.id_producto
-group by productos.nombre_producto;
-
-
-/*
- Escribe una función que dado el ID de un pedido devuelva el precio total de ese pedido,
- calculándolo a partir de detalles_pedido
- */
-
- select pedidos.id_pedido, detalles_pedido.subtotal from pedidos
- right join detalles_pedido on pedidos.id_pedido = detalles_pedido.id_pedido
- group by pedidos.id_pedido;
-
+select productos.id_producto, productos.nombre_producto,
+sum(detalles_pedido.subtotal) / sum(detalles_pedido.cantidad) as precioMedio from
+productos left join detalles_pedido using (id_producto)
+group by productos.id_producto, productos.nombre_producto;
