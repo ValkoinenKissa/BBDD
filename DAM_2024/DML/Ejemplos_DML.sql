056fb5b595343b14e3ab889da12ca9fa7fc38f44
Alberto Bollo <albertobollo7@protonmail.com>
2025-04-14 23:45:37 +0200

    Ejemplos DML

--- DAM_2024/DML/Ejemplos_DML.sql
+++ DAM_2024/DML/Ejemplos_DML.sql
@@ -7,3 +7,10 @@ insert into sala values (3, 2, 393);
 
 
 select * from sala;
+
+/*
+Como ya sabemos, en la base de datos de MCGRAWCINES tenemos los 44 cines de la provincia de Valencia.
+ Ahora debes modificar la estructura de la tabla para crear un nuevo campo que nos permita guardar
+la web oficial de cada uno de los cines, y luego actualizar el contenido de esa tabla poniendo sus webs.
+¿Cómo lo harías?
+ */
