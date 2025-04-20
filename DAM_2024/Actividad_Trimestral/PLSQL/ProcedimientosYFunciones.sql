36dbb1fe2df57548b2f0e1080e521d5f93cdfc4e
Alberto Bollo <albertobollo7@protonmail.com>
2025-04-20 03:32:23 +0200

    Ejercicios funciones actividad trmestral

--- DAM_2024/Actividad_Trimestral/PLSQL/ProcedimientosYFunciones.sql
+++ DAM_2024/Actividad_Trimestral/PLSQL/ProcedimientosYFunciones.sql
@@ -43,3 +43,69 @@ delimiter ;
 -- El id 103 nos mostrara el total de pagos del ciente 103 --> 22314.36
 
 select mostrarPagosCliente(103);
+
+/*
+ 3.	Crea una función que reciba un número de cliente (customerNumber) y devuelva la suma de todos sus pagos si el
+ customerNumber es par o el número total de pagos si el customerNumber es impar.
+ */
+
+delimiter //
+
+create or replace function mostarPagosClienteParImpar(idCliente int)
+    returns decimal(10, 2)
+begin
+    declare sumaPagos decimal(10, 2);
+    select sum(amount) into sumaPagos from payments where customerNumber = idCliente;
+    if idCliente % 2 = 0 then
+        return sumaPagos;
+    else
+        select count(payments.checkNumber) into sumaPagos from payments where customerNumber = idCliente;
+        return sumaPagos;
+    end if;
+
+end;
+
+delimiter ;
+
+-- Numero par, muestra la suma de todos sus pagos
+
+select mostarPagosClienteParImpar(112) as sumaDeTodosLosPagos;
+
+
+-- Numero impar, muestra el numero total de pagos de el cliente
+
+
+select mostarPagosClienteParImpar(103) as totalPagosCliente;
+
+
+/*
+ 4.	Crea una función que para un número entero cualquiera N calcule sumatorio de 1 a N, esto es sumar todos los números
+ enteros desde 1 hasta N (incluido). Si el número introducido es <= 0 debe devolver 0.
+ */
+
+delimiter //
+create or replace function sumaHastaN(numeroN int)
+    returns int
+begin
+    declare contador int default 0;
+    declare sumatorio int default 0;
+    if numeroN <= 0 then
+        return 0;
+    else
+        bucle:
+        while contador < numeroN
+            do
+                set contador = contador + 1;
+                set sumatorio = sumatorio + contador;
+            end while;
+        return sumatorio;
+    end if;
+end;
+
+delimiter ;
+
+-- La suma de todos los numeros desde el 1 hasta el 5 es: 15
+
+select sumaHastaN(5) as sumaDeTodosLosNumeros;
+
+
